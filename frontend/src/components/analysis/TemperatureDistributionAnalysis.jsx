7d3dc70421ab593d41eee1f24d26c05777eaada9
Sebastian Fast <s.fast@cognigy.com>
2025-07-09 22:10:00 +0200

    Refactor import statement to streamline React usage

--- frontend/src/components/analysis/TemperatureDistributionAnalysis.jsx
+++ frontend/src/components/analysis/TemperatureDistributionAnalysis.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import ContentSplit from '../layout/ContentSplit';
 import TemperaturePercentogram from '../charts/TemperaturePercentogram';
