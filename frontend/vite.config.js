8a12e5c5bd73fdedcf0ec88ed85b6d1d2b345994
Sebastian Fast <sebastian.fast@posteo.net>
2025-08-19 21:08:16 +0200

    Fix proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/',
     server: {
         proxy: {
-            '/': 'https://esistwarm.jetzt/'
+            '/data/': 'https://esistwarm.jetzt',
+            '/station_data/': 'https://esistwarm.jetzt'
         }
     },
     plugins: [
         react(),
+        tailwindcss(),
         nodePolyfills({
             // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
             include: ['process'],
