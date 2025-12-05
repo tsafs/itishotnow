3f15b9f4fbdcec773b0c9017f2438d703185ec5d
Sebastian Fast <sebastian.fast@posteo.net>
2025-12-05 10:55:19 +0100

    Remove unused import of useSelectedCityName in TemperatureAnomaliesByDayOverYears component

--- frontend/src/components/plots/TemperatureAnomaliesByDayOverYears/View.tsx
+++ frontend/src/components/plots/TemperatureAnomaliesByDayOverYears/View.tsx
@@ -1,7 +1,6 @@
 import { createPlotView } from '../../common/PlotView/createPlotView.js';
 import TemperatureAnomaliesByDayOverYearsLeftSide from './LeftSide.js';
 import TemperatureAnomaliesByDayOverYearsRightSide from './RightSide.js';
-import { useSelectedCityName } from '../../../store/hooks/hooks.js';
 
 const TemperatureAnomaliesByDayOverYears = createPlotView({
     leftContent: TemperatureAnomaliesByDayOverYearsLeftSide,
